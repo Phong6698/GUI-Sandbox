73f3e8be1d6319d981c2242e81d485d89dff4fb6
Chiramet Phong Penglerd <Phong@hotmail.ch>
2017-12-03 20:42:31 +0100

    wunderfull bootstrap layouting :tada:

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import 'hammerjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  players = [
+    {name: 'Player 1'},
+    {name: 'Player 2'},
+    {name: 'Player 3'},
+    {name: 'Player 4'},
+    {name: 'Player 5'},
+    {name: 'Player 6'},
+    {name: 'Player 7'},
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NoopAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material';
 
 
 @NgModule({
@@ -12,7 +15,10 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot(),
+    NoopAnimationsModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
